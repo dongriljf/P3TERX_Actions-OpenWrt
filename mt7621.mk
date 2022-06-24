27d5089c345b9c32fc6b680a446bffe41b99ac7e
dongriljf <41283005+dongriljf@users.noreply.github.com>
2022-06-24 13:15:39 +0800

    fix2

--- mt7621.mk
+++ mt7621.mk
@@ -1841,19 +1841,18 @@ TARGET_DEVICES += zyxel_wap6805
 
 define Device/zte_e8820s
   $(Device/dsa-migration)
-  $(Device/uimage-lzma-loader)
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
   UBINIZE_OPTS := -E 5
-  IMAGE_SIZE := 128768k
+  IMAGE_SIZE := 120320k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
-	check-size
+  IMAGE/factory.bin := append-kernel | pad-to $(KERNEL_SIZE) | append-ubi | \
+        check-size
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820S
-  DEVICE_PACKAGES := kmod-mt760e kmod-mt76x2e kmod-usb3 \
-	kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-mt76 kmod-mt7603 kmod-mt76x2 kmod-usb3 \
+          kmod-usb-ledtrig-usbport wpad-basic
 endef
 TARGET_DEVICES += zte_e8820s

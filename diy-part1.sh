f421395a2bffc1c34aea637060f580cc1e8d409c
dongriljf <41283005+dongriljf@users.noreply.github.com>
2023-10-03 09:03:04 +0800

    11

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

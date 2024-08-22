80928fb9494f526d13f3462c43851cbe77d00060
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-08-22 16:42:06 +0800

    test

--- diy_ipq6018.sh
+++ diy_ipq6018.sh
@@ -13,8 +13,8 @@
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-
 #rm -rf feeds/smpackage/{*openclash,*homeproxy,*ikoolproxy}
+rm -rf feeds/luci/applications/{luci-app-bypass,luci-app-homeproxy,luci-app-mosdns,luci-app-passwall*,luci-app-ssr-plus}
 ./scripts/feeds install -a  
 
 
